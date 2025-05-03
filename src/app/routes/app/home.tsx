65e9c2d8c2abc3ae487b400097fb1ba652d245f5
hmuto111 <h.muto111@gmail.com>
2025-05-04 01:45:26 +0900

    feat: カードの実装 #7

--- src/app/routes/app/home.tsx
+++ src/app/routes/app/home.tsx
@@ -1,9 +1,58 @@
+import { Card } from "@/features/blog/components/card/card";
+import { Article } from "@/features/blog/types/article";
+
 const Home = () => {
-  return (
-    <div>
-      Homeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-    </div>
-  );
+  const articledata: Article[] = [
+    {
+      id: 1,
+      title: "Pythonむずすぎ",
+      img: "none",
+      tag: ["python", "RAG", "javascript", "java", "typescript", "react"],
+      view: 100,
+      post: new Date("2025-05-03T14:03:29.000Z"),
+    },
+    {
+      id: 2,
+      title: "Pythonむずすぎ",
+      img: "none",
+      tag: ["python", "RAG"],
+      view: 100,
+      post: new Date("2025-05-03T14:03:29.000Z"),
+    },
+    {
+      id: 3,
+      title: "Pythonむずすぎ",
+      img: "none",
+      tag: ["python", "RAG"],
+      view: 100,
+      post: new Date("2025-05-03T14:03:29.000Z"),
+    },
+    {
+      id: 4,
+      title: "Reactむずすぎ",
+      img: "@/assets/react.svg",
+      tag: ["React", "RAG"],
+      view: 100,
+      post: new Date("2025-05-03T14:03:29.000Z"),
+    },
+    {
+      id: 5,
+      title: "Pythonむずすぎ",
+      img: "none",
+      tag: ["python", "RAG"],
+      view: 100,
+      post: new Date("2025-05-03T14:03:29.000Z"),
+    },
+    {
+      id: 6,
+      title: "Pythonむずすぎ",
+      img: "none",
+      tag: ["python", "RAG"],
+      view: 100,
+      post: new Date("2025-05-03T14:03:29.000Z"),
+    },
+  ];
+  return <Card article={articledata[0]} />;
 };
 
 export default Home;
---
+++ src/features/blog/components/card/card.tsx
@@ -0,0 +1,39 @@
+import styles from "./card.module.css";
+import { Article } from "../../types/article";
+import HimajinLogo from "@/assets/himajin-logo.png";
+
+type Props = {
+  article: Article;
+};
+
+export const Card = ({ article }: Props) => {
+  const postDate = article.post;
+  const year = postDate.getFullYear();
+  const month = String(postDate.getMonth() + 1).padStart(2, "0");
+  const day = String(postDate.getDate()).padStart(2, "0");
+  const formattedPostDate = `${year}/${month}/${day}`;
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.article_img}>
+        {article.img !== "none" ? (
+          <img src={article.img} alt={article.title} />
+        ) : (
+          <img src={HimajinLogo} alt="Default Img" />
+        )}
+      </div>
+      <div className={styles.article_description}>
+        <div className={styles.article_title}>{article.title}</div>
+        <div className={styles.tags}>
+          {article.tag.map((t) => (
+            <div className={styles.tag}>{t}</div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.article_info}>
+        <div className={styles.info}>{`閲覧数  ${article.view}`}</div>
+        <div className={styles.info}>{`投稿日  ${formattedPostDate}`}</div>
+      </div>
+    </div>
+  );
+};
