2d798c9bd64284608d22953cc84a483ea5099544
hmuto111 <h.muto111@gmail.com>
2025-05-14 15:47:42 +0900

    feat: モックデータを返すAPIを叩いて、フロントで受信データを表示させる #17

---
+++ frontend/src/lib/api-client.ts
@@ -0,0 +1,32 @@
+import Axios from "axios";
+import type { InternalAxiosRequestConfig } from "axios";
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = "application/json";
+  }
+
+  config.withCredentials = true;
+  return config;
+}
+
+export const api = Axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+  withCredentials: true,
+});
+
+api.interceptors.request.use(authRequestInterceptor);
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // const message = error.response?.data?.message || error.message;
+
+    // 認証エラー時処理
+    //if (error.response?.status === 401) {
+    //}
+
+    return Promise.reject(error);
+  }
+);
