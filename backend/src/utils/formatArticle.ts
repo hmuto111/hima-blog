ce5ffec2a6cfe621eda1e3e76d87b895139ba8f9
hmuto111 <h.muto111@gmail.com>
2025-05-17 17:40:23 +0900

    feat: フォーマット関数をより共通化する #18

--- backend/src/utils/formatArticle.ts
+++ backend/src/utils/formatArticle.ts
@@ -5,26 +5,26 @@ import type {
 } from "../routes/web/types/article";
 import { dateToString } from "./date-to-string";
 
+const transformArticle = (
+  article: Article,
+  tags: Tag[]
+): ArticleContentType => ({
+  ...article,
+  tag: article.tag.map((t) => tags.find((tag) => tag.id === t)?.name as string),
+  post: dateToString(article.post),
+  updated: dateToString(article.updated),
+});
+
 export const formatArticle = (
   article: Article[],
   tags: Tag[]
 ): ArticleContentType[] => {
-  return article.map((a) => ({
-    ...a,
-    tag: a.tag.map((t) => tags.find((tag) => tag.id === t)?.name as string),
-    post: dateToString(a.post),
-    updated: dateToString(a.updated),
-  }));
+  return article.map((a) => transformArticle(a, tags));
 };
 
-export const formatArticleContent = (article: Article, tags: Tag[]) => {
-  const formattedArticleContent = {
-    ...article,
-    tag: article.tag.map(
-      (t) => tags.find((tag) => tag.id === t)?.name as string
-    ),
-    post: dateToString(article.post),
-    updated: dateToString(article.updated),
-  };
-  return formattedArticleContent;
+export const formatArticleContent = (
+  article: Article,
+  tags: Tag[]
+): ArticleContentType => {
+  return transformArticle(article, tags);
 };
