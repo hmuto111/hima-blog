251c7b7cac6831efc60e0ffe44e0828b3b7bbfcf
hmuto111 <h.muto111@gmail.com>
2025-05-31 18:29:02 +0900

    feat: routerに認証ルートを追加 #29

--- backend/src/routes/admin/adminRouter.ts
+++ backend/src/routes/admin/adminRouter.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-//import { auth } from "./api/auth";
+import auth from "./api/auth";
 import article from "./api/article";
 
 export const adminRouter = new Hono()
@@ -16,5 +16,5 @@ export const adminRouter = new Hono()
     })
   )
   .basePath("/v1")
-  .route("/article", article);
-//.route("/auth", auth)
+  .route("/article", article)
+  .route("/auth", auth);
